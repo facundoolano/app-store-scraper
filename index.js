2085abc61257f5adbfdc11d57a4f38e5cc5f51c0
Facundo Olano <facundo.olano@gmail.com>
2016-06-05 16:44:01 -0300

    added similar method

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ module.exports.app = require('./lib/app');
 module.exports.list = require('./lib/list');
 module.exports.search = require('./lib/search');
 module.exports.suggest = require('./lib/suggest');
+module.exports.similar = require('./lib/similar');
