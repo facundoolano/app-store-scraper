9547681304a9d213fac5b31a210e1fdf77a454ca
Facundo Olano <facundo.olano@gmail.com>
2016-09-03 20:28:49 -0300

    added reviews method

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const c = require('./lib/constants');
 module.exports.category = c.category;
 module.exports.collection = c.collection;
 module.exports.device = c.device;
+module.exports.sort = c.sort;
 
 module.exports.app = require('./lib/app');
 module.exports.list = require('./lib/list');
