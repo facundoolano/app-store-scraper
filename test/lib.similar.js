e8451a78e3069449662f2fbcfe4801f70a0f1f90
Piercus <piercus@gmail.com>
2016-12-13 10:33:32 +0100

    add country to similar

--- test/lib.similar.js
+++ test/lib.similar.js
@@ -8,4 +8,8 @@ describe('Similar method', () => {
     return store.similar({id: '553834731'})
     .then((apps) => apps.map(assertValidApp));
   });
+  it('should fetch a valid application list in fr country', () => {
+    return store.similar({id: '553834731', country: 'fr'})
+    .then((apps) => apps.map(assertValidApp));
+  });
 });
