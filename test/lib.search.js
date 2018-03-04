29c53d052548cd755d4f6c785ec30e99aa9f1557
Facundo Olano <facundo.olano@gmail.com>
2018-03-04 11:09:21 -0300

    add search pagination

--- test/lib.search.js
+++ test/lib.search.js
@@ -10,6 +10,19 @@ describe('Search method', () => {
     .then((apps) => apps.map(assertValidApp));
   });
 
+  it('should properly paginate results', () => {
+    const p1 = store.search({term: 'Panda', num: 10});
+    const p2 = store.search({term: 'Panda', num: 10, page: 2});
+    return Promise.all([p1, p2])
+      .then(([apps1, apps2]) => {
+        assert.equal(10, apps1.length);
+        assert.equal(10, apps2.length);
+        apps1.map(assertValidApp);
+        apps2.map(assertValidApp);
+        assert.notEqual(apps1[0].appId, apps2[0].appId);
+      });
+  });
+
   it('should fetch a valid application list in fr country', () => {
     return store.search({country: 'fr', term: 'Panda vs Zombies'})
     .then((apps) => {
