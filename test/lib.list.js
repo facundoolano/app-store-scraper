7e118c6cfdcf7a4d91b3293c41d7c7d34e4efbea
Kolt Ewing <koltewing@gmail.com>
2018-11-17 11:21:14 -0500

    Update apple url for list method (#79)
    * update list function to use updated apple response url and data format
    
    * re-add free app assertion
    
    * remove arrays around genre and genreId
    
    * re-add developerId
    
    * re-add collection and category
    
    * update README with list category documentation
    
    * more readme updates

--- test/lib.list.js
+++ test/lib.list.js
@@ -48,10 +48,10 @@ describe('List method', () => {
     .then((apps) => apps.map((app) => {
       assert.isString(app.description);
 
-      assert.equal(app.genres[0], 'Games');
-      assert.equal(app.genreIds[0], '6014');
+      assert.equal(app.genre, 'Games');
+      assert.equal(app.genreId, '6014');
 
-      assert.equal(app.price, '0');
+      assert.equal(app.price, '0.00000');
       assert(app.free);
 
       assert.isString(app.developer);
