ba42b7f40aa6ed8299d883c94fb4db75ac38c0d9
Christian Fei <crifei93@gmail.com>
2019-04-01 17:38:57 +0200

    test(ratings): add tests for country

--- test/lib.ratings.js
+++ test/lib.ratings.js
@@ -3,9 +3,11 @@
 const assert = require('chai').assert;
 const store = require('../index');
 
+const id = '553834731'
+
 describe('Ratings method', () => {
-  it('should fetch valid ratings data', () => {
-    return store.ratings({id: '553834731'})
+  it('should fetch valid ratings data by id', () => {
+    return store.ratings({id})
       .then((ratings) => {
         assert.isObject(ratings)
         assert.isNumber(ratings.ratings)
@@ -15,6 +17,17 @@ describe('Ratings method', () => {
         assert.isNumber(ratings.histogram['3'])
         assert.isNumber(ratings.histogram['4'])
         assert.isNumber(ratings.histogram['5'])
+      });
+  });
+
+  it('should fetch valid ratings data by id and country', () => {
+    let ratingsForUs, ratingsForFr
+    return store.ratings({id})
+      .then((ratings) => {ratingsForUs = ratings})
+      .then(() => store.ratings({id, country: 'fr'}))
+      .then((ratings) => {ratingsForFr = ratings})
+      .then(() => {
+        assert.notDeepEqual(ratingsForUs, ratingsForFr)
       });
   });
 });
