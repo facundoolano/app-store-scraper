19f3517d75eda38f18b507ab32ceb8cb84b5126e
Christian Fei <crifei93@gmail.com>
2019-04-01 17:50:08 +0200

    fix(ratings): handle missing appId

--- test/lib.ratings.js
+++ test/lib.ratings.js
@@ -3,31 +3,46 @@
 const assert = require('chai').assert;
 const store = require('../index');
 
-const id = '553834731'
+const id = '553834731';
+const appId = 'com.midasplayer.apps.candycrushsaga';
 
 describe('Ratings method', () => {
   it('should fetch valid ratings data by id', () => {
     return store.ratings({id})
       .then((ratings) => {
-        assert.isObject(ratings)
-        assert.isNumber(ratings.ratings)
-        assert.isObject(ratings.histogram)
-        assert.isNumber(ratings.histogram['1'])
-        assert.isNumber(ratings.histogram['2'])
-        assert.isNumber(ratings.histogram['3'])
-        assert.isNumber(ratings.histogram['4'])
-        assert.isNumber(ratings.histogram['5'])
+        assert.isObject(ratings);
+        assert.isNumber(ratings.ratings);
+        assert.isObject(ratings.histogram);
+        assert.isNumber(ratings.histogram['1']);
+        assert.isNumber(ratings.histogram['2']);
+        assert.isNumber(ratings.histogram['3']);
+        assert.isNumber(ratings.histogram['4']);
+        assert.isNumber(ratings.histogram['5']);
+      });
+  });
+
+  it('should fetch valid ratings data by appId', () => {
+    return store.ratings({appId})
+      .then((ratings) => {
+        assert.isObject(ratings);
+        assert.isNumber(ratings.ratings);
+        assert.isObject(ratings.histogram);
+        assert.isNumber(ratings.histogram['1']);
+        assert.isNumber(ratings.histogram['2']);
+        assert.isNumber(ratings.histogram['3']);
+        assert.isNumber(ratings.histogram['4']);
+        assert.isNumber(ratings.histogram['5']);
       });
   });
 
   it('should fetch valid ratings data by id and country', () => {
-    let ratingsForUs, ratingsForFr
+    let ratingsForUs, ratingsForFr;
     return store.ratings({id})
-      .then((ratings) => {ratingsForUs = ratings})
+      .then((ratings) => {ratingsForUs = ratings;})
       .then(() => store.ratings({id, country: 'fr'}))
-      .then((ratings) => {ratingsForFr = ratings})
+      .then((ratings) => {ratingsForFr = ratings;})
       .then(() => {
-        assert.notDeepEqual(ratingsForUs, ratingsForFr)
+        assert.notDeepEqual(ratingsForUs, ratingsForFr);
       });
   });
 });
